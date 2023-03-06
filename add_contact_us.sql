20b32e5d834adf90cb5d3515874983db9a09d492
Ali Mansouri <alimansi1382@gmail.com>
2023-03-06 17:24:43 +0330

    insert_delete_contact_us

---
+++ add_contact_us.sql
@@ -0,0 +1,6 @@
+INSERT INTO `contact_us`(`full_name`, `email`, `subject`, `message`) VALUES (
+    'mohamad alavi',
+    'mohmad.alavi.987@gmail.com',
+    'Include an email and phone number so visitors can get in touch with you on their first attempt.',
+    'Generally, visitors can also leave feedback or ask questions through these channels. You’ll receive valuable information about your customers’ preferences and expectations if done correctly.'
+)
